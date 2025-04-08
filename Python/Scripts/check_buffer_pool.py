636e5879d9c6d0b89885ed3aade48da86f10a444
Amy Smith <asmith@liquibase.com>
2025-04-08 08:34:23 -0500

    Made changes for multiple values found for lists

--- Python/Scripts/check_buffer_pool.py
+++ Python/Scripts/check_buffer_pool.py
@@ -4,9 +4,11 @@
 ###
 ###
 ### This script throws the following errors:
-### 1) Default Buffer Pool Not Found for Database {database_name}.
-### 2) Buffer Pool Not Found in CREATE TABLESPACE script.
-### 3) CREATE TABLESPACE Buffer Pool (buffer_name) must match the default Buffer Pool (default_buffer_pool) for the database (database_name).
+### 1) Multiple CREATE TABLESPACE statements found in changeset. Only one CREATE TABLESPACE allowed per changeset.
+### 2) Default Buffer Pool Not Found for Database {database_name}.
+### 3) Multiple BUFFER POOL statements found in CREATE TABLESPACE statement. Only one Buffer Pool can be specified.
+### 4) Buffer Pool Not Found in CREATE TABLESPACE script.
+### 5) CREATE TABLESPACE Buffer Pool (buffer_name) must match the default Buffer Pool (default_buffer_pool) for the database (database_name).
 ###
 ### Sample Tablespace:
 ###
@@ -81,66 +83,83 @@ for change in changes:
     ### Look for database regex in SQL
     ###
     database_list = re.findall(regex_pattern_database, raw_sql)
-    database_name = ''.join(database_list)
- 
-    ### 
-    ### End check if script does not contain regex pattern
-    ###    
-    if database_name is None or database_name == '':
+    
+    if len(database_list) > 1:
+        liquibase_status.fired = True                  
+        status_message = f"Multiple CREATE TABLESPACE statements found in changeset. Only one CREATE TABLESPACE allowed per changeset."
+        liquibase_status.message = status_message
+        sys.exit(1)
         break
     else:
-        ### print(f"Database Name: {database_name}")
+        database_name = ''.join(database_list)
         
-        ###
-        ### Execute query to get the default buffer pool for the database
-        ###
-        sql_query = f"SELECT BPOOL FROM SYSIBM.SYSDATABASE WHERE NAME = '{database_name}'"
-        default_buffer_pool_list = liquibase_utilities.query_for_list(sql_query, None, ";")
-        
-        if len(default_buffer_pool_list) == 0:
-            ### print(f"Default Buffer Pool Not Found for Database {database_name}")
-            
-            liquibase_status.fired = True                  
-            status_message = f"Default Buffer Pool Not Found for Database {database_name}."
-            liquibase_status.message = status_message
-            sys.exit(1)
+        ### 
+        ### End check if script does not contain regex pattern
+        ###    
+        if database_name is None or database_name == '':
             break
         else:
-        
-            default_buffer_pool = default_buffer_pool_list [0]["BPOOL"].strip()
-            ### print(f"Default Buffer Pool: {default_buffer_pool}")
+            ### print(f"Database Name: {database_name}")
             
             ###
-            ### Look for bufferpool regex in SQL
+            ### Execute query to get the default buffer pool for the database
             ###
-            buffer_pool_list = re.findall(regex_pattern_bufferpool, raw_sql)
-            buffer_pool = ''.join(buffer_pool_list)
+            sql_query = f"SELECT BPOOL FROM SYSIBM.SYSDATABASE WHERE NAME = '{database_name}'"
+            default_buffer_pool_list = liquibase_utilities.query_for_list(sql_query, None, ";")
             
-            if buffer_pool is None or buffer_pool == '':
-                ### print(f"Buffer Pool Not Found in script {buffer_pool}")
-               
+            if len(default_buffer_pool_list) == 0:
+                ### print(f"Default Buffer Pool Not Found for Database {database_name}")
+                
                 liquibase_status.fired = True                  
-                status_message = f"Buffer Pool Not Found in CREATE TABLESPACE script."
+                status_message = f"Default Buffer Pool Not Found for Database {database_name}."
                 liquibase_status.message = status_message
                 sys.exit(1)
                 break
-                
             else:
-                ### print(f"Buffer Pool in script: {buffer_pool}")
+            
+                default_buffer_pool = default_buffer_pool_list [0]["BPOOL"].strip()
+                ### print(f"Default Buffer Pool: {default_buffer_pool}")
                 
                 ###
-                ### Check that the buffer pool values match
+                ### Look for bufferpool regex in SQL
                 ###
+                buffer_pool_list = re.findall(regex_pattern_bufferpool, raw_sql)
                 
-                if buffer_pool != default_buffer_pool:
-                    
-                    liquibase_status.fired = True                    
-                    status_message = str(liquibase_utilities.get_script_message()).replace("__BUFFER_POOL__", f"{buffer_pool}")
-                    status_message = status_message.replace("__DEFAULT_BUFFER_POOL__", f"{default_buffer_pool}")
-                    status_message = status_message.replace("__DATABASE_NAME__", f"{database_name}")
+                if len(buffer_pool_list) > 1:
+                    liquibase_status.fired = True                  
+                    status_message = f"Multiple BUFFER POOL statements found in CREATE TABLESPACE statement. Only one Buffer Pool can be specified."
                     liquibase_status.message = status_message
                     sys.exit(1)
                     break
+                else:
+                
+                    buffer_pool = ''.join(buffer_pool_list)
+                    
+                    if buffer_pool is None or buffer_pool == '':
+                        ### print(f"Buffer Pool Not Found in script {buffer_pool}")
+                       
+                        liquibase_status.fired = True                  
+                        status_message = f"Buffer Pool Not Found in CREATE TABLESPACE script."
+                        liquibase_status.message = status_message
+                        sys.exit(1)
+                        break
+                        
+                    else:
+                        ### print(f"Buffer Pool in script: {buffer_pool}")
+                        
+                        ###
+                        ### Check that the buffer pool values match
+                        ###
+                        
+                        if buffer_pool != default_buffer_pool:
+                            
+                            liquibase_status.fired = True                    
+                            status_message = str(liquibase_utilities.get_script_message()).replace("__BUFFER_POOL__", f"{buffer_pool}")
+                            status_message = status_message.replace("__DEFAULT_BUFFER_POOL__", f"{default_buffer_pool}")
+                            status_message = status_message.replace("__DATABASE_NAME__", f"{database_name}")
+                            liquibase_status.message = status_message
+                            sys.exit(1)
+                            break
 
 ###
 ### Default return code
